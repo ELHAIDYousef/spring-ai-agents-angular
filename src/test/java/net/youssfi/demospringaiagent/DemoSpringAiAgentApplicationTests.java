b42a3c08ed9897780e7bf750cd980172c44bfb51
ELHAID Yousef <elhaidyousef@gmail.com>
2025-12-28 12:45:45 +0100

    vcc project

---
+++ src/test/java/net/youssfi/demospringaiagent/DemoSpringAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.youssfi.demospringaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
